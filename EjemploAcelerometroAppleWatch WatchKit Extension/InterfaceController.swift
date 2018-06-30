ee8033af8f31e8aed1a94949f4a8ae9cc7650253
codelapp <info@codelapp.com>
2018-06-30 09:58:47 +0200

    Commit final

--- EjemploAcelerometroAppleWatch WatchKit Extension/InterfaceController.swift
+++ EjemploAcelerometroAppleWatch WatchKit Extension/InterfaceController.swift
@@ -12,17 +12,30 @@ class InterfaceController: WKInterfaceController {
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+        motionManager.accelerometerUpdateInterval = 0.1
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        if motionManager.isAccelerometerAvailable {
+            let handler: CMAccelerometerHandler = {(datos: CMAccelerometerData?, error: Error?) -> Void in
+                self.labelX.setText(String(datos!.acceleration.x))
+                self.labelY.setText(String(datos!.acceleration.y))
+                self.labelZ.setText(String(datos!.acceleration.z))
+            }
+            motionManager.startAccelerometerUpdates(to: OperationQueue.current!, withHandler: handler)
+        } else {
+            self.labelX.setText("No disponible")
+            self.labelY.setText("No disponible")
+            self.labelZ.setText("No disponible")
+        }
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+        motionManager.stopAccelerometerUpdates()
     }
 
 }
